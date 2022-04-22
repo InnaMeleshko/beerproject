b82812976d9e5627a03b6d757114deb312cc9277
InnaMeleshko <Iamlayout@gmail.com>
2022-04-22 14:11:33 +0200

    made three buttons

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <button
+          className="App-link"
+          style={{fontSize: "2em"}}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          0.25l
+        </button>
+
+        <button
+          className="App-link"
+          style={{fontSize: "2em"}}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          0.33l
+        </button>
+        <button
+          className="App-link"
+          style={{fontSize: "2em"}}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          0.50l
+        </button>
+     
+     
+     
+    </div>
+    
+  );
+}
+
+export default App;
