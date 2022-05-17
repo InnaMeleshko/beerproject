a90ca2860449182ae82ec32e6ae62748bd4eb5c9
InnaMeleshko <Iamlayout@gmail.com>
2022-05-17 09:35:04 +0200

    made all buttons and span with total result

--- src/App.js
+++ src/App.js
@@ -1,43 +1,123 @@
+import "./App.scss";
+import "./button/button.scss";
 
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+  };
+
+  const [counter2, setCounter2] = useState(0);
+
+  const handleClick2 = () => {
+    setCounter2(counter2 + 1);
+  };
+
+  const [counter3, setCounter3] = useState(0);
+
+  const handleClick3 = () => {
+    setCounter3(counter3 + 1);
+  };
+
+  const [total, setTotal] = useState(0);
+
+  const handlTotal = (volume) => {
+    setTotal(total + volume);
+  };
+
   return (
-    <div className="App">
-        <button
-          className="App-link"
-          style={{fontSize: "2em"}}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          0.25l
-        </button>
+    <div className="Container">
+      <div className="Container-inner">
+        <h1 className="Greeting">How much beer are u going to drink?</h1>
+        <div className="App">
+          <div className="Buttons-block">
+            <button
+              className="Counter"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              You had {counter} beers{" "}
+            </button>
 
-        <button
-          className="App-link"
-          style={{fontSize: "2em"}}
+            <button
+              onClick={() => {
+                handlTotal(0.25);
+                handleClick();
+              }}
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              0.25l
+            </button>
+          </div>
+
+          <div className="Buttons-block">
+            <button
+              className="Counter"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              You had {counter2} beers{" "}
+            </button>
+            <button
+              onClick={() => {
+                handlTotal(0.33);
+                handleClick2();
+              }}
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              0.33l
+            </button>
+          </div>
+
+          <div className="Buttons-block">
+            <button
+              className="Counter"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              You had {counter3} beers{" "}
+            </button>
+            <button
+              onClick={() => {
+                handlTotal(0.5);
+                handleClick3();
+              }}
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              0.50l
+            </button>
+          </div>
+        </div>
+
+        <span
+          className="total"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          0.33l
-        </button>
-        <button
-          className="App-link"
-          style={{fontSize: "2em"}}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          0.50l
-        </button>
-     
-     
-     
+          U had {total} liters in common, {counter + counter2 + counter3} beers
+        </span>
+      </div>
     </div>
-    
   );
-}
+};
 
 export default App;
